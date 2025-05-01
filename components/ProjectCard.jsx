f24a07a6e69e94d6fa1e09dd22fc9b91ffaf75bf
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2025-04-30 23:42:44 -0400

    Edited styling

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import ProjectButtons from "./ProjectButtons";
+import { abril_fatface } from "../styles/fonts.jsx"
 import style from "../styles/components.module.css"
 
 function ProjectCard({project}){
-return(
-  <div className={style.project_card}>
-    <Image src= {project.image} 
-      width={200}
-      height={200} />
-    <div className={style.project_data}>
-      <div className={style.project_title} >
-        <h5>{project.title}</h5>
-        <ProjectButtons url1={project.url1} url2={project.url2} />
-      </div>
-      <div className={style.project_description}>
-        {project.description}
+  return(
+    <div className={style.project_card}>
+      <Image src= {project.image} 
+        width={200}
+        height={200} />
+      <div className={style.project_card_data}>
+        <div id={style.project_card_title} className={abril_fatface.className} >
+          <h4>{project.title}</h4>
+          <ProjectButtons url1={project.url1} url2={project.url2} />
+        </div>
+        <div className={style.project_description}>
+          {project.description}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default ProjectCard
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,14 +1,19 @@
 import info from "../content/info.json";
 import ProjectCard from "./ProjectCard";
+import { special_elite } from "@/styles/fonts";
 import style from "../styles/components.module.css"
 
 function Projects(){
   return(
-    <div id='Projects' className={style.project} >
-      {info.Projects.map((project) => 
-        <ProjectCard project={project} />
-      )}
+    <div id='Projects' >
+      <h3 id={style.project_title} className={special_elite.className}>PROJECTS</h3>
+      <div>
+        {info.Projects.map((project) => 
+          <ProjectCard project={project} />
+        )}
+      </div>
     </div>
+
   );
 }
 
